dcb8f344c9763cf2cfda750e413293aa247c662c
abrham alemu <ergetie14@gmail.com>
2025-10-20 21:28:21 +0300

    first

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db/dbConfig");
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 const userRouter = require("./routes/userRoute");
 const questionRouter = require("./routes/questionsRoute");
 const answerRouter = require("./routes/answerRoute");
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.json({ response: "success" });
 });
 
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
 app.use("/api/question", authMiddlewares, questionRouter);
 app.use("/api/answer", authMiddlewares, answerRouter);
 app.use("/api/ai", authMiddlewares, aiRouter);
