ea27e7850081d11f29d8be6ce44876763885580f
abrham alemu <ergetie14@gmail.com>
2025-10-21 09:05:05 +0300

    commit

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+
+// const dbConnection = mysql.createPool({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+//   connectionLimit: 10,
+// });
+
+// module.exports = dbConnection.promise();
+const mysql = require("mysql2/promise");
 
 const dbConnection = mysql.createPool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
 });
 
-module.exports = dbConnection.promise();
+module.exports = dbConnection;
